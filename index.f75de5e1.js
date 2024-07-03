5cb9038883fdc7632cee623f7a0a67ad98035454
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-03 11:21:45 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const listOfEmployee = [
+    ...document.querySelectorAll("li")
+];
+const parentElement = document.querySelector("ul");
+const sortList = (elements)=>{
+    return elements.sort((a, b)=>{
+        const salaryA = Number(a.getAttribute("data-salary").replace(/(\$|,)/g, ""));
+        const salaryB = Number(b.getAttribute("data-salary").replace(/(\$|,)/g, ""));
+        return salaryB - salaryA;
+    });
+};
+const sortedElements = sortList(listOfEmployee);
+parentElement.innerHTML = "";
+sortedElements.forEach((element)=>{
+    parentElement.appendChild(element);
+});
+const getEmployees = (list)=>{
+    return list.map((element)=>{
+        const attributes = [
+            ...element.attributes
+        ];
+        const user = {};
+        for (const attr of attributes)user[attr.name.slice(5)] = attr.value;
+        user.name = element.innerText;
+        return user;
+    });
+};
+window.console.log(getEmployees(listOfEmployee));
+
+//# sourceMappingURL=index.f75de5e1.js.map
